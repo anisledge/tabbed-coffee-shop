4fead5397504b13e789905e33abfdb64a12f6368
Ani Sledgianowski <ani.sledge@gmail.com>
2016-05-16 19:31:21 -0600

    multi-line strings

--- script.js
+++ script.js
@@ -1,59 +1,69 @@
-//add an image
-//copy about how great the restaurant is
-//headline
-//append each element to the #content div
-//use tabs to access the Contact and Menu pages
-//do not use a separate page, change the content using jQuery
-//when you click a tab, the JS will remove that tabs elements, 
-//and add the new tabs elements
-
-//document ready
-
-var genRow = function(row_type, hex_type) {
-	var row = $("<div class='hex-row " + row_type + "'></div>");
-	row.append( genHex(hex_type) );
-	row.append( genHex(hex_type) );
+var genRow = function(len) {
+	var row = $("<div class='hex-row'></div>");
+	for (i = 0; i < len; i = i + 1) {
+		row.append(genHex());
+	}
 	return row;
 }
-var genHex = function(hex_type) {
-	return $("<div class='hex hexagon " + hex_type + "'><div class='inner'></div></div>");
+var genHex = function() {
+	return $("<div class='hex hexagon'><div class='inner'></div></div>");
 }
 
-var genHome = function(content_list) {
+var genHome = function() {
 	var home = $("<section id='hexagons'></section>");
 	var grid = $("<div id='hex-grid'></div>");
-	var first_row = genRow("odd", "top");
-	var second_row = genRow("even", "middle");
-	second_row.children().last().attr('id', 'about');
-	second_row.append(genHex("middle"));
-	var third_row = genRow("odd", "bottom");
+	var first_row = genRow(2);
+	var second_row = genRow(3);
+	var third_row = genRow(2);
+	first_row.add(third_row).addClass('odd');
+	second_row.children().eq(1).attr('id', 'about');
 	grid.append(first_row).append(second_row).append(third_row);
+	
+	var content = ["<img src='images/espresso.png' alt='espresso'>", 
+						 "<img src='images/coffee.png' alt='coffee'>", 
+						 "<img src='images/donut.png' alt='donuts'>", 
+						 'At our Coffee Shop, we want to inspire and enlighten you.\
+						  Come to our Coffee Shop in Colorado Springs for the best \
+						  coffee and atmosphere.', 
+						 "<img src='images/fudge.png' alt='fudge'>", 
+						 "<img src='images/sandwich.png' alt='sandwiches'>", 
+						 "<img src='images/tea.png' alt='tea'>"]
+	
 	grid.find(".inner").html( function(index, oldText) {
-		$(this).html(content_list[index]);
+		$(this).html(content[index]);
 	});
 
 	home.append(grid);
 	return home
 }
 
+
 var genContact = function() {
-	var contact = $("<section id='contact' class='square'>  </section>");
-	contact.append("<h2>Contact</h2>");
-	contact.append("<p>Phone us: (719) 333-3333</p>");
-	contact.append("<p>Email us: example@example.com</p>");
-	contact.append("<p>Find us: <a href='#'>Google Maps</a></p>");
+	var contact = $("<section id='contact' class='square'>\
+		<h2>Contact</h2>\
+		<p>Phone us: (719) 333-3333</p>\
+		<p>Email us: example@example.com</p>\
+		<p>Find us: <a href='#'>Google Maps</a></p>\
+	    </section>");
 	return contact
 }
-var genMenu = function(menu_items) {
-	var menu = $("<section id='menu' class='square'></section>");
-	menu.append("<h2>Menu</h2>");
-	
-	var table = $("<table></table>");
-	var tbody = $("<tbody></tbody>");
-	for (i = 0; i < menu_items.length; i = i + 1) {
-		tbody.append("<tr><td>" + menu_items[i] + "</td><td>$9.99</td></tr>");
-	}
-	table.append(tbody);
+
+var genMenu = function() {
+	var menu = $("<section id='menu' class='square'>\
+		<h2>Menu</h2></section>");
+	var table = $("<table><tbody>\
+		<tr><td>Americano</td>$4.99</tr>\
+		<tr><td>Espresso</td>$4.99</tr>\
+		<tr><td>Cafe Late</td>$4.99</tr>\
+		<tr><td>Caramel Mocchiato</td>$5.99</tr>\
+		<tr><td>Doppio</td>$4.99</tr>\
+		<tr><td>Hot Chocolate</td>$5.99</tr>\
+		<tr><td>Drip Coffee</td>$2.99</tr>\
+		<tr><td>Hot Tea</td>$3.99</tr>\
+		<tr><td>Fudge</td>$5.99</tr>\
+		<tr><td>Single Donut</td>$2.99</tr>\
+		<tr><td>Sandwich</td>$7.99</tr>\
+		</tbody></table>");
 	menu.append(table);
 	return menu
 }
@@ -64,22 +74,12 @@ $(function() {
 	var menuLink = $( "#menu-link" );
 	var contactLink = $( "#contact-link");
 	var homeLink = $( "#home-link" );
-	var menu_items = ['Americano', 'Espresso', 'Cafe Late', 'Caramel Mocchiato', 
-				  'Doppio', 'Hot Chocolate', 'Drip Coffee', 'Hot Tea', 'Fudge', 'Single Donut']
-	var index_content = ["<img src='images/espresso.png' alt='espresso'>", 
-						 "<img src='images/coffee.png' alt='coffee'>", 
-						 "<img src='images/donut.png' alt='donuts'>", 
-						 'At our Coffee Shop, we want to inspire and enlighten you. Come to our Coffee Shop in Colorado Springs for the best coffee and atmosphere.', 
-						 "<img src='images/fudge.png' alt='fudge'>", 
-						 "<img src='images/sandwich.png' alt='sandwiches'>", 
-						 "<img src='images/tea.png' alt='tea'>"]
-
-
-	var contact = genContact();
-	var home = genHome(index_content);
-	var menu = genMenu(menu_items);
 	
+	var home = genHome();
 	main.append(home);
+	
+	var contact = genContact();
+	var menu = genMenu();
 
 	contactLink.click(function() {
 		if (active != 'contact') {
